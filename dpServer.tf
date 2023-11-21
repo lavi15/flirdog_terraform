915659173aa2badfdf60f6b2a7a3579b418f91df
lavi15 <biz.jongin@gmail.com>
2023-11-21 22:15:27 +0900

    2023.11.21

--- dpServer.tf
+++ dpServer.tf
@@ -1,3 +1,15 @@
+#Script
+resource "ncloud_init_script" "kafka-main-script" {
+  name    = var.kafka_main_script.name
+  content = var.kafka_main_script.content
+}
+
+resource "ncloud_init_script" "kafka-init-script" {
+  name    = var.kafka_init_script.name
+  content = var.kafka_init_script.content
+}
+
+
 #Interface
 resource "ncloud_network_interface" "dp_connect_server_interface" {
   name                  = "dp-connect-server-interface1"
@@ -10,6 +22,7 @@ resource "ncloud_network_interface" "kafka_private_interface1" {
   name                  = "kafka-private-interface1"
   description           = "kafka-private-interface1"
   subnet_no             = ncloud_subnet.dp_subnet.id
+  private_ip            = var.kafka_server.kafka-cluster1-ip
   access_control_groups = [ncloud_access_control_group.dp_acg.id]
 }
 
@@ -17,6 +30,7 @@ resource "ncloud_network_interface" "kafka_private_interface2" {
   name                  = "kafka-private-interface2"
   description           = "kafka-private-interface2"
   subnet_no             = ncloud_subnet.dp_subnet.id
+  private_ip            = var.kafka_server.kafka-cluster2-ip
   access_control_groups = [ncloud_access_control_group.dp_acg.id]
 }
 
@@ -24,16 +38,19 @@ resource "ncloud_network_interface" "kafka_private_interface3" {
   name                  = "kafka-private-interface3"
   description           = "kafka-private-interface3"
   subnet_no             = ncloud_subnet.dp_subnet.id
+  private_ip            = var.kafka_server.kafka-cluster3-ip
   access_control_groups = [ncloud_access_control_group.dp_acg.id]
 }
+
 
 #server
 resource "ncloud_server" "kafka_server_1" {
   subnet_no = ncloud_subnet.dp_subnet.id
   name      = "flirdog-kafka1"
   server_product_code = var.kafka_server.server_product_code
-  member_server_image_no = var.kafka_server.member_server_image_no
+  member_server_image_no = var.kafka_server.kafka-cluster1-image
   login_key_name = var.loginkey
+  init_script_no = ncloud_init_script.kafka-main-script.id
   network_interface {
     network_interface_no = ncloud_network_interface.kafka_private_interface1.id
     order                = 0
@@ -44,8 +61,9 @@ resource "ncloud_server" "kafka_server_2" {
   subnet_no = ncloud_subnet.dp_subnet.id
   name      = "flirdog-kafka2"
   server_product_code = var.kafka_server.server_product_code
-  member_server_image_no = var.kafka_server.member_server_image_no
+  member_server_image_no = var.kafka_server.kafka-cluster2-image
   login_key_name = var.loginkey
+  init_script_no = ncloud_init_script.kafka-init-script.id
   network_interface {
     network_interface_no = ncloud_network_interface.kafka_private_interface2.id
     order                = 0
@@ -56,13 +74,15 @@ resource "ncloud_server" "kafka_server_3" {
   subnet_no = ncloud_subnet.dp_subnet.id
   name      = "flirdog-kafka3"
   server_product_code = var.kafka_server.server_product_code
-  member_server_image_no = var.kafka_server.member_server_image_no
+  member_server_image_no = var.kafka_server.kafka-cluster3-image
   login_key_name = var.loginkey
+  init_script_no = ncloud_init_script.kafka-init-script.id
   network_interface {
     network_interface_no = ncloud_network_interface.kafka_private_interface3.id
     order                = 0
   }
 }
+
 
 resource "ncloud_server" "dp_connect_server" {
   subnet_no = ncloud_subnet.was_subnet.id
--- dpServerVariable.tf
+++ dpServerVariable.tf
@@ -3,6 +3,40 @@ variable "kafka_server" {
 
   default = {
     server_product_code = "SVR.VSVR.HICPU.C032.M064.NET.SSD.B050.G002"
-    member_server_image_no = "20635034"
+    kafka-cluster1-ip = "10.0.3.16"
+    kafka-cluster2-ip = "10.0.3.17"
+    kafka-cluster3-ip = "10.0.3.18"
+    kafka-cluster1-image = "20652792"
+    kafka-cluster2-image = "20652829"
+    kafka-cluster3-image = "20652839"
+  }
+}
+
+variable "kafka_main_script" {
+  type = map(string)
+
+  default = {
+    name = "kafka-main-script"
+    content = <<-EOF
+#!/bin/bash
+nohup ~/confluent-7.0.1/bin/zookeeper-server-start -daemon ~/confluent-7.0.1/etc/kafka/zookeeper.properties
+nohup ~/confluent-7.0.1/bin/kafka-server-start -daemon ~/confluent-7.0.1/etc/kafka/server.properties
+nohup ~/confluent-7.0.1/bin/connect-distributed -daemon ~/confluent-7.0.1/etc/kafka/connect-distributed.properties
+~/confluent-7.0.1/bin/kafka-topics --create --bootstrap-server 10.0.3.16:9092 --topic chat
+EOF
+  }
+}
+
+variable "kafka_init_script" {
+  type = map(string)
+
+  default = {
+    name = "kafka-init-script"
+    content = <<-EOF
+#!/bin/bash
+nohup ~/confluent-7.0.1/bin/zookeeper-server-start -daemon ~/confluent-7.0.1/etc/kafka/zookeeper.properties
+nohup ~/confluent-7.0.1/bin/kafka-server-start -daemon ~/confluent-7.0.1/etc/kafka/server.properties
+nohup ~/confluent-7.0.1/bin/connect-distributed -daemon ~/confluent-7.0.1/etc/kafka/connect-distributed.properties
+EOF
   }
 }
